1c70d353a93370803093899088338f1fa6caea4a
May2Beez <sk83505@stud.uph.edu.pl>
2021-06-05 16:20:00 +0200

    Decreased CPU usage by replacing Template Matching with my own detecting pixels

--- pond_game.py
+++ pond_game.py
@@ -34,8 +34,8 @@ def detect_color(rgb, img):
 
     pix = pix.load()
 
-    for x in range(0, img.shape[1] - 1):
-        for y in range(0, img.shape[0] - 1):
+    for y in range(0, img.shape[0]):
+        for x in range(0, img.shape[1]):
             if pix[x, y] in rgb:
                 return True
     return False
@@ -49,7 +49,10 @@ class PondGame:
         self.start_pond = vision.Vision(cv2.imread(resource_path("images/start_pond.png")))
         self.score_checker = check_score.CheckScore(
             cv2.imread(resource_path("images/score_digits.png"), cv2.IMREAD_GRAYSCALE))
-        self.bat_pixel_rgb = [(2, 0, 3), (2, 2, 4)]
+        self.bat_pixel_rgb = [(33, 36, 170), (32, 35, 170), (39, 49, 210)]
+        self.catch_rgb = [(255, 247, 198), ]
+        self.combo_fish_rgb = [(1, 218, 255), ]
+        self.result_window_rgb = [(247, 168, 150), ]
         self.playing = False
         self.failed = False
         self.window_rect = win32gui.GetWindowRect(self.NosTale_hwnd)
@@ -82,6 +85,8 @@ class PondGame:
             if score > int(self.score_levels[int(self.reward_level)]):
                 self.playing = False
                 self.failed = False
+
+            time.sleep(1)
 
     # Checks if game window is opened
     def if_pond_start_exists(self):
@@ -118,7 +123,6 @@ class PondGame:
         self.left_bot_bob = vision.Vision(cv2.imread(resource_path("images/bob_left_bot.png")))
         self.right_top_bob = vision.Vision(cv2.imread(resource_path("images/bob_top_right.png")))
         self.result_window = vision.Vision(cv2.imread(resource_path("images/result_window.png")))
-        self.get_reward = vision.Vision(cv2.imread(resource_path("images/get_reward.png")))
 
         if self.left_bot_bob and self.right_top_bob:
             time.sleep(0.1)
@@ -132,26 +136,20 @@ class PondGame:
             time.sleep(0.60)
 
     # Checks if bat is over bob
-    def check_bat_over_bob(self, img, the_way, key):
-        search_bat = img[int(the_way[1] - 25):int(the_way[1] - 10), int(the_way[0] - 15):int(the_way[0] + 5)].copy()
+    def check_bat_over_bob(self, img, key):
 
-        if not detect_color(self.bat_pixel_rgb, search_bat):
+        if not detect_color(self.bat_pixel_rgb, img):
             self.click(key)
 
     # Main functionality
     def play_game(self):
-        combo_fish = vision.Vision(cv2.imread(resource_path("images/combo_fish.png")))
         img = self.NosTale_window.get_screenshot()
 
-        x = img.shape[1] / 2 - 350 / 2
-        y = img.shape[0] / 2 - 200 / 2
-        bobs = img[int(y):int(y + 200), int(x):int(x + 350)].copy()
-
         while True:
-            not_moving_left, not_moving_bot = self.left_bot_bob.find(bobs, threshold=0.95)
-            not_moving_top, not_moving_right = self.right_top_bob.find(bobs, threshold=0.95)
+            left_bob, bot_bob = self.left_bot_bob.find(img, threshold=0.95)
+            top_bob, right_bob = self.right_top_bob.find(img, threshold=0.95)
 
-            if not_moving_left and not_moving_bot and not_moving_right and not_moving_top:
+            if left_bob and bot_bob and top_bob and right_bob:
                 break
 
         self.score = Thread(target=self.checking_score_thread, args=())
@@ -165,50 +163,37 @@ class PondGame:
             except Exception:
                 continue
 
-            x = img.shape[1] / 2 - 350 / 2
-            y = img.shape[0] / 2 - 200 / 2
-            bobs = img[int(y):int(y + 200), int(x):int(x + 350)].copy()
-
-            # IMG for game window
-            crop_x = img.shape[1] / 2 - 550 / 2
-            crop_y = img.shape[0] / 2 - 480 / 2
-            game_img = img[int(crop_y):int(crop_y + 480), int(crop_x):int(crop_x + 550)].copy()
+            left_bob_img = img[int(left_bob[1] - 19):int(left_bob[1] - 16),
+                           int(left_bob[0] + 8):int(left_bob[0] + 14)].copy()
+            bot_bob_img = img[int(bot_bob[1] - 19):int(bot_bob[1] - 16),
+                          int(bot_bob[0] + 8):int(bot_bob[0] + 14)].copy()
+            top_bob_img = img[int(top_bob[1] - 19):int(top_bob[1] - 16),
+                          int(top_bob[0] + 8):int(top_bob[0] + 14)].copy()
+            right_bob_img = img[int(right_bob[1] - 19):int(right_bob[1] - 16),
+                            int(right_bob[0] + 8):int(right_bob[0] + 14)].copy()
 
             # IMG for combo fish
-            combo_x = game_img.shape[1] / 2 - 500 / 2
-            combo_y = game_img.shape[0] / 2 - 100 / 2
-            combo_fish_crop_img = game_img[int(combo_y):int(combo_y + 100), int(combo_x):int(combo_x + 500)].copy()
+            combo_fish_crop_img = img[int(left_bob[1] - 16):int(left_bob[1] - 13),
+                                  int(left_bob[0] - 30):int(left_bob[0] - 25)].copy()
 
-            if combo_fish.find(combo_fish_crop_img, threshold=0.90):
+            if detect_color(self.combo_fish_rgb, combo_fish_crop_img):
 
                 self.solve_combo_fish()
                 time.sleep(1)
 
             else:
 
-                left_bot = self.left_bot_bob.find(bobs, threshold=0.90)
+                if detect_color(self.catch_rgb, left_bob_img):
+                    self.check_bat_over_bob(left_bob_img, win32con.VK_LEFT)
 
-                if not_moving_left not in left_bot or not_moving_bot not in left_bot:
+                if detect_color(self.catch_rgb, bot_bob_img):
+                    self.check_bat_over_bob(bot_bob_img, win32con.VK_DOWN)
 
-                    if not_moving_left not in left_bot:
+                if detect_color(self.catch_rgb, top_bob_img):
+                    self.check_bat_over_bob(top_bob_img, win32con.VK_UP)
 
-                        self.check_bat_over_bob(bobs, not_moving_left, win32con.VK_LEFT)
-
-                    elif not_moving_bot not in left_bot:
-
-                        self.check_bat_over_bob(bobs, not_moving_bot, win32con.VK_DOWN)
-
-                else:
-
-                    right_top = self.right_top_bob.find(bobs, threshold=0.90)
-
-                    if not_moving_top not in right_top:
-
-                        self.check_bat_over_bob(bobs, not_moving_top, win32con.VK_UP)
-
-                    elif not_moving_right not in right_top:
-
-                        self.check_bat_over_bob(bobs, not_moving_right, win32con.VK_RIGHT)
+                if detect_color(self.catch_rgb, right_bob_img):
+                    self.check_bat_over_bob(right_bob_img, win32con.VK_RIGHT)
 
                 # Full IMG
                 try:
@@ -217,17 +202,17 @@ class PondGame:
                     continue
 
                 # IMG for result window
-                result_window_x = fail_img.shape[1] / 2 - 400 / 2
-                result_window_y = fail_img.shape[0] / 2 - 200 / 2
-                result_window_crop_img = fail_img[int(result_window_y):int(result_window_y + 200),
-                                         int(result_window_x):int(result_window_x + 400)].copy()
+                result_window_x = fail_img.shape[1] / 2 - 5
+                result_window_y = fail_img.shape[0] / 2 - 60
+                result_window_crop_img = fail_img[int(result_window_y):int(result_window_y + 10),
+                                         int(result_window_x):int(result_window_x + 10)].copy()
 
-                if self.result_window.find(result_window_crop_img, threshold=0.90):
+                if detect_color(self.result_window_rgb, result_window_crop_img):
                     self.failed = True
                     self.playing = False
                     break
 
-            #time.sleep(0.1)
+            time.sleep(0.05)
 
         time.sleep(0.1)
 
@@ -243,43 +228,45 @@ class PondGame:
             while not self.failed:
 
                 try:
-                    img = self.NosTale_window.get_screenshot()
+                    fail_img = self.NosTale_window.get_screenshot()
                 except Exception:
                     continue
 
-                # IMG for game window
-                crop_x = img.shape[1] / 2 - 550 / 2
-                crop_y = img.shape[0] / 2 - 480 / 2
-                game_img = img[int(crop_y):int(crop_y + 480), int(crop_x):int(crop_x + 550)].copy()
-
                 # IMG for result window
-                result_window_x = game_img.shape[1] / 2 - 400 / 2
-                result_window_y = game_img.shape[0] / 2 - 200 / 2
-                result_window_crop_img = game_img[int(result_window_y):int(result_window_y + 200),
-                                         int(result_window_x):int(result_window_x + 400)].copy()
+                result_window_x = fail_img.shape[1] / 2 - 5
+                result_window_y = fail_img.shape[0] / 2 - 60
+                result_window_crop_img = fail_img[int(result_window_y):int(result_window_y + 10),
+                                         int(result_window_x):int(result_window_x + 10)].copy()
 
-                if self.result_window.find(result_window_crop_img, threshold=0.90):
+                if detect_color(self.result_window_rgb, result_window_crop_img):
                     break
 
                 self.click(win32con.VK_LEFT, False)
 
                 time.sleep(0.5)
 
+            get_reward = vision.Vision(cv2.imread(resource_path("images/get_reward.png")))
+
             while True:
 
-                reward_points = self.get_reward.find(img, threshold=0.95)
+                try:
+                    img = self.NosTale_window.get_screenshot()
+                except Exception:
+                    continue
+
+                reward_points = get_reward.find(img, threshold=0.95)
 
                 if reward_points:
                     break
 
-                time.sleep(0.1)
+                time.sleep(1)
 
             # Click Reward button
             lParam = win32api.MAKELONG(reward_points[0][0], reward_points[0][1] + 20)
             win32gui.SendMessage(self.NosTale_hwnd, win32con.WM_MOUSEMOVE, None, lParam)
             win32gui.SendMessage(self.NosTale_hwnd, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, lParam)
             win32gui.SendMessage(self.NosTale_hwnd, win32con.WM_LBUTTONUP, None, lParam)
-            time.sleep(1.5)
+            time.sleep(1.2)
 
             # Click level reward
             reward_button_y = int(img.shape[0] / 2 + 50)
@@ -291,7 +278,7 @@ class PondGame:
             win32gui.SendMessage(self.NosTale_hwnd, win32con.WM_MOUSEMOVE, None, lParam)
             win32gui.SendMessage(self.NosTale_hwnd, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, lParam)
             win32gui.SendMessage(self.NosTale_hwnd, win32con.WM_LBUTTONUP, None, lParam)
-            time.sleep(1.5)
+            time.sleep(1.2)
 
             self.repeats_counter += 1
 
@@ -313,13 +300,15 @@ class PondGame:
                 if chosen_option_points:
                     break
 
+                time.sleep(1)
+
             time.sleep(0.1)
 
             lParam = win32api.MAKELONG(chosen_option_points[0][0], chosen_option_points[0][1] + 20)
             win32gui.SendMessage(self.NosTale_hwnd, win32con.WM_MOUSEMOVE, None, lParam)
             win32gui.SendMessage(self.NosTale_hwnd, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, lParam)
             win32gui.SendMessage(self.NosTale_hwnd, win32con.WM_LBUTTONUP, None, lParam)
-            time.sleep(1.5)
+            time.sleep(1.2)
 
             end_img = self.NosTale_window.get_screenshot()
 
@@ -410,6 +399,6 @@ class PondGame:
                 self.click(win32con.VK_DOWN, False)
                 combo += 1
 
-            time.sleep(0.03)
+            time.sleep(0.1)
 
         time.sleep(0.2)
